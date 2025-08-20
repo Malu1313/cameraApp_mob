c8783dfdad376f0f88e2c56809c4627092ee99e3
Malu1313 <marialuisavinholireis@gmail.com>
2025-08-20 11:17:59 -0300

    first commit

---
+++ src/homeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    const handleCam = () => {
+        navigation.navigate("Cam");
+    }
+    return (
+        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+            <Button title="Abrir Câmera" onPress={handleCam} color="pink"/>
+        </View>
+    )
+}
+export default HomeScreen
