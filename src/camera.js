d7b9ce6b0d8c3e06ba50e3c2a096704b263e0d55
Malu1313 <marialuisaviholireis@gmail.com>
2025-09-03 10:22:17 -0300

    03-09

--- src/camera.js
+++ src/camera.js
@@ -1,4 +1,5 @@
 import { CameraView, useCameraPermissions } from "expo-camera";
+import { createEvento } from "../axios/api";
 import { useRef, useState } from "react";
 import {
   Button,
@@ -20,13 +21,14 @@ export default function Cam() {
   if (!permission) return <View />;
 
   if (!permission.granted) {
-    return(
-    <View style={styles.container}>
-      <Text style={{ textAlign: "center" }}>
-        Precisamos de permissão para exibir a câmera
-      </Text>
-      <Button onPress={requestPermission} title="Permitir" />
-    </View>)
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          Precisamos de permissão para exibir a câmera
+        </Text>
+        <Button onPress={requestPermission} title="Permitir" />
+      </View>
+    );
   }
 
   async function takePicture() {
@@ -34,12 +36,27 @@ export default function Cam() {
       const data = await camRef.current.takePictureAsync();
       setCapturedPhoto(data.uri);
       setOpen(true);
+
+      // Simular um formulario
+      const form = {
+        nome: "Evento teste",
+        descricao: "Evento teste Image Mobile",
+        data_hora: "2015-09-03 09:00:00",
+        local: "Franca",
+        fk_id_organizador: 1,
+      };
+      try{
+        const response = await createEvento(form,data.uri)
+        console.log("Evento criado", response.data)
+        }catch(error){
+          console.log("Error", error.response.data.error)
+        }
     }
   }
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} ref={camRef} >
+      <CameraView style={styles.camera} ref={camRef}>
         <TouchableOpacity style={styles.buttonTake} onPress={takePicture}>
           <MaterialIcons name="camera" size={60} color="white" />
         </TouchableOpacity>
--- src/homeScreen.js
+++ src/homeScreen.js
@@ -1,39 +1,39 @@
 import React from "react";
 import { View, Button } from "react-native";
-import {useNavigation} from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
-import api from "./axios/axios"
 
 const HomeScreen = () => {
-    const [stateLED, setStateLED] = useState({value: false});
-    const navigation = useNavigation();
-    const handleCam= () => {
-        navigation.navigate("Cam");
+  const [stateLED, setStateLED] = useState({ value: false });
+  const navigation = useNavigation();
+
+  async function ledToggle() {
+    try {
+      // Faz a requisição da API da Adafruit
+      const response = await api.toggleLED({ value: `${!stateLED.value}` });
+      setStateLED({ value: !stateLED.value });
+      console.log("Resposta:", response.data);
+    } catch (error) {
+      console.log("Erro", error.response.data);
     }
+  }
 
-    async function ledToggle(){
-        try{
-            //Faz a requisição da API adafruit
-            const response = await api.toggleLED({value: `${!stateLED.value}`});
-            setStateLED({value: !stateLED.value})
-            console.log("resposta: ", response.data);
-        }
-        catch(error){
-            console.log("Erro", error.response.data);
-        }
-    }
-
-    return(
-        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-            <View style={{marginBottom: 20}}>
-            <Button title="Abrir camera" onPress={handleCam} color='blue'/>
-            </View>
-            <View>
-                <Button title={stateLED.value ? "Desligar LED" : "Ligar LED"} onPress={ledToggle} color={stateLED.value ? "red" : "green"}/>
-            </View>
-        </View>
-    )
-
-}
-
-export default HomeScreen
+  const handleCam = () => {
+    navigation.navigate("Cam");
+  };
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ marginBottom: 20 }}>
+        <Button title="Abrir Câmera" onPress={handleCam} color="pink" />
+      </View>
+      <View>
+        <Button
+          title={stateLED.value ? "Desligar o Led" : "Ligar LED"}
+          onPress={ledToggle}
+          color={stateLED.value ? "red" : "green"}
+        />
+      </View>
+    </View>
+  );
+};
+export default HomeScreen;
